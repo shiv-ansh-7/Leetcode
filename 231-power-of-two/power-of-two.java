ece7f41e951b7b5abbc60b8ad9eb0d7885277b41
Shivansh Chaurasiya <shivanshchaurasiya51@gmail.com>
2026-08-25 22:42:11 +0530

    Time: 1 ms (96.61%) | Memory: 42.3 MB (94.43%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n<1){
+            return false ;
+
+        }
+        while(n%2==0){
+            n = n/2;
+
+        }
+        return n==1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
