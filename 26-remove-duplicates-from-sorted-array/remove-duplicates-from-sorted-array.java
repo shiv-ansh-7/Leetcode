713ccb7071be911bf8554cb96370f8f4b6de4958
Shivansh Chaurasiya <shivanshchaurasiya51@gmail.com>
2026-09-09 22:22:35 +0530

    Time: 1 ms (77.00%) | Memory: 46.5 MB (81.24%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+      if(nums.length == 0) return 0;
+      int res = 1;
+      for(int i = 1; i < nums.length; i++){
+        if(nums[i] != nums[i - 1]){
+          nums[res] = nums[i];
+          res++;
+        }
+      }
+      return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
