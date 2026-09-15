e0f7726f901f4bfcfaf38bd3a54d967bb29bd0b2
Shivansh Chaurasiya <shivanshchaurasiya51@gmail.com>
2026-09-15 20:39:56 +0530

    Time: 4 ms (50.45%) | Memory: 268.9 MB (17.16%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+
+       
+        reverse(nums, 0, n - 1);
+
+    
+        reverse(nums, 0, k - 1);
+
+      
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
